4d408bebb29151bca6f7f032793af162bf5485bd
Lucas Nseyep <lucas.nseyep@gmail.com>
2023-02-23 15:42:12 +0000

    List logic + Bookmarks controller

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,9 +1,12 @@
 class Movie < ApplicationRecord
-  validates :title, presence: true
-  validates :overview, presence: true
-  validates :poster_url, presence: true
+  has_many :bookmarks
 
-  validates :rating, presence: true, numericality: { only_integer: true }
-  validates :rating, numericality: { greater_than_or_equal_to: 0 }
-  validates :rating, numericality: { less_than_or_equal_to: 10 }
+  validates :title, presence: true
+  validates :title, uniqueness: true
+  validates :overview, presence: true, uniqueness: true
+  # validates :poster_url, presence: true
+
+  # validates :rating, presence: true, numericality: { only_integer: true }
+  # validates :rating, numericality: { greater_than_or_equal_to: 0 }
+  # validates :rating, numericality: { less_than_or_equal_to: 10 }
 end
