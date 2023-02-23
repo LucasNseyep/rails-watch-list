8687642f4013c00a916ec915af937763ad91e386
Lucas Nseyep <lucas.nseyep@gmail.com>
2023-02-23 13:58:31 +0000

    Movie model validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,9 @@
 class Movie < ApplicationRecord
+  validates :title, presence: true
+  validates :overview, presence: true
+  validates :poster_url, presence: true
+
+  validates :rating, presence: true, numericality: { only_integer: true }
+  validates :rating, numericality: { greater_than_or_equal_to: 0 }
+  validates :rating, numericality: { less_than_or_equal_to: 10 }
 end
